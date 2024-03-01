9a1791fdab464ab72389524d01886d8ffc6c9d20
Casey Wesolowski <95983025+CaseyWesolowski@users.noreply.github.com>
2024-02-29 22:59:43 -0500

    Create Admin.js
    basic admin main view, still missing API stuff

---
+++ src/Admin.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Admin.css';
+import { Link } from 'react-router-dom';
+
+function Admin() {
+  return (
+    <div>
+
+    <nav className="navbar">
+        <div className="navbar-left">
+          <span>E-CINEMA</span>
+        </div>
+        <div className="navbar-center">
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="navbar-right">
+          <a href="/registereduser"> Home </a>
+        </div>
+      </nav>
+
+      <div className="main-content">
+        <div className="admin-option">
+        <button><Link to="/managemovies">Manage Movies</Link></button>
+          <p>Add, edit, or delete movies</p>
+          {/* add movie api stuff below each */}
+        </div>
+        <div className="admin-option">
+          <h3>Manage Users</h3>
+          <p>View and manage user accounts</p>
+         
+        </div>
+        <div className="admin-option">
+          <button><Link to="/managepromotions">Manage Promotions</Link></button>
+          <p>Create, edit, or delete promotions</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
