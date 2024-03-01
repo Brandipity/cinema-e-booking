eb727c5c32b12247c9488aad07ccc6ad79e0fc0a
Brandon Winston <bwinston@udscc.com>
2024-03-01 09:58:38 -0500

    fixed background tiling

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,19 @@
 // Movie.js
 // we love components.
+
+import { useNavigate } from 'react-router-dom';
+
 function Movie({ movie }) {
+    const navigate = useNavigate();
+
+
+    // placeholder
+    const handleMovieClick = () => {
+        navigate('/ticketbooking', { state: { movie } });
+    };
     return (
         <div className="movie">
-            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} style={{ width: '200px' }} />
+            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} onClick={handleMovieClick} style={{ width: '200px' }} />
             <h3>{movie.title}</h3>
             <p>{movie.overview}</p>
         </div>
