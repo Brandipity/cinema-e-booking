c76bd61f561cdabbe475006008b264851bafcc93
Brandon Winston <bwinston@udscc.com>
2024-03-19 11:48:17 -0400

    - Updated comments and variables names

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 
 // server.js
+// Contains endpoints to interact with the database securely
 
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
@@ -12,32 +13,34 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-// initialize the SQLite database
+// Initialize the SQLite database
+// Make sure the Express server is running
+
 const db = new sqlite3.Database('./backend/cinema.db', (err) => {
     if (err) {
         return console.error(err.message);
     }
-    console.log('Connected to the cinema SQLite database.');
+    console.log('Connected to the SQLite database.');
 });
 
 // add-a-movie endpoint
-app.post('/api/movies', (req, res) => {
-    const { id, title, description, releaseDate } = req.body;
+app.post('/api/movies', (request, response) => {
+    const { id, title, description, releaseDate } = request.body;
     const sql = `INSERT INTO movies (tmdbId, title, description, release_date) VALUES (?, ?, ?, ?)`;
 
     db.run(sql, [id, title, description, releaseDate], function(err) {
         if (err) {
             console.error(err.message);
-            res.status(500).json({ error: err.message });
+            response.status(500).json({ error: err.message });
             return;
         }
-        res.json({ message: 'Movie added successfully', movieId: this.lastID });
+        response.json({ message: 'Movie added successfully', movieId: this.lastID });
     });
 });
 
-// returns an array with all the movies in the database, kinda disgusting but whatever
+// returns an array with all the movies in the database, kinda disgusting but whatever (should add a 'Now Playing' attribute later)
 
-app.get('/api/movies/now-playing', (req, res) => {
+app.get('/api/movies/now-playing', (request, response) => {
     const sql = `SELECT * FROM movies`;
     // wrapped in a promise call, API will return null otherwise
     new Promise((resolve, reject) => {
@@ -50,11 +53,11 @@ app.get('/api/movies/now-playing', (req, res) => {
         });
     })
         .then(movies => {
-            res.json(movies);
+            response.json(movies);
         })
         .catch(err => {
             console.error(err.message);
-            res.status(500).send("Failed to fetch current movies");
+            response.status(500).send("Failed to fetch current movies");
         });
 });
 
