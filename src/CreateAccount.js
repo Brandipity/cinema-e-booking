b16275799926ae9cbbd0189b78e13cd1b0d3a06a
alliepinto899 <ajp71860@uga.edu>
2024-05-06 23:02:14 -0400

    fixed hopefully

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './CreateAccount.css';
 import axios from 'axios';
 
+
 import {sendConfirmationEmail} from './emailSender';
 
 function CreateAccount() {
--- src/emailSender.js
+++ src/emailSender.js
@@ -1,6 +1,6 @@
 import emailjs from 'emailjs-com';
 
-async function sendPromotionEmail(promoCode) {
+export async function sendPromotionEmail(promoCode) {
     const promotionContent = `We have added a new promotion! Use promo code ${promoCode} for discounts.`;
 
     const templateParams = {
@@ -15,4 +15,19 @@ async function sendPromotionEmail(promoCode) {
     }
 }
 
-export default sendPromotionEmail;
+export async function sendConfirmationEmail(email, username, confirmationToken) {
+    const confirmationUrl = `http://localhost:3000/confirm/${confirmationToken}`;
+
+    const templateParams = {
+        to_email: email,
+        username: username,
+        confirmation_url: confirmationUrl,
+    };
+
+    try {
+        await emailjs.send('service_pfnvmol', 'template_uyzt4iv', templateParams, '0yAX7vO20pm2Hs_Ye');
+        console.log('Confirmation email sent successfully');
+    } catch (error) {
+        console.error('Error sending confirmation email:', error);
+    }
+}
